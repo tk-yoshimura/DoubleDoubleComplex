396bfb32840d385d7a021225967e8182af387003
tk-yoshimura <yoshimura8192@gmail.com>
2022-09-04 22:05:44 +0900

    Update QuaternionFunctionTests.cs

--- DoubleDoubleComplexTest/Quaternion/QuaternionFunctionTests.cs
+++ DoubleDoubleComplexTest/Quaternion/QuaternionFunctionTests.cs
@@ -19,8 +19,8 @@ namespace DoubleDoubleComplexTests {
             foreach (Quaternion q in new[] { 1, 2, 4, 5 }) {
                 Quaternion s = Quaternion.Sin(q), c = Quaternion.Cos(q);
 
-                QuaternionAssert.AreEqual(ddouble.Sin(q.R), s.R, 1e-23);
-                QuaternionAssert.AreEqual(ddouble.Cos(q.R), c.R, 1e-23);
+                QuaternionAssert.AreEqual(ddouble.Sin(q.R), s.R, 1e-25);
+                QuaternionAssert.AreEqual(ddouble.Cos(q.R), c.R, 1e-25);
             }
 
             foreach (Quaternion q in new[] { (1, 2, 3, 4), (2, 5, -2, 6), (6, -3, 1, 2), (7, -4, 5, 1), (3, -9, 2, 4), (7, 1, -3, 2), (-3, 5, 2, -1) }) {
