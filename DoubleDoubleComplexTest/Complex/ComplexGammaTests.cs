2e96b9a4007555c19b1a1357b9f3ae04858ecb37
tk-yoshimura <yoshimura8192@gmail.com>
2023-09-13 01:02:57 +0900

    add gamma

---
+++ DoubleDoubleComplexTest/Complex/ComplexGammaTests.cs
@@ -0,0 +1,15 @@
+﻿using DoubleDoubleComplex;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DoubleDoubleComplexTests {
+    [TestClass()]
+    public class ComplexGammaTests {
+        [TestMethod()]
+        public void SandboxTest() {
+            Complex c = Complex.Gamma((1, 16));
+
+            Console.WriteLine(c);
+        }
+    }
+}
