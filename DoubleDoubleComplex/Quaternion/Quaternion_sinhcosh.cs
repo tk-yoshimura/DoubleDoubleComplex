b5f4e3e4c60c3899b604e504226a0567aadd9ff8
tk-yoshimura <yoshimura8192@gmail.com>
2024-08-23 02:41:13 +0900

    add: sinh,cosh,tanh

---
+++ DoubleDoubleComplex/Quaternion/Quaternion_sinhcosh.cs
@@ -0,0 +1,23 @@
+﻿namespace DoubleDoubleComplex {
+
+    public partial class Quaternion {
+
+        public static Quaternion Sinh(Quaternion q) {
+            Quaternion q_pexp = Exp(q), q_mexp = Exp(-q);
+
+            return (q_pexp - q_mexp) / 2d;
+        }
+
+        public static Quaternion Cosh(Quaternion q) {
+            Quaternion q_pexp = Exp(q), q_mexp = Exp(-q);
+
+            return (q_pexp + q_mexp) / 2d;
+        }
+
+        public static Quaternion Tanh(Quaternion q) {
+            Quaternion q_pexp = Exp(q), q_mexp = Exp(-q);
+
+            return (q_pexp - q_mexp) / (q_pexp + q_mexp);
+        }
+    }
+}
---
+++ DoubleDoubleComplexTest/Quaternion/QuaternionSinhCoshTests.cs
@@ -0,0 +1,41 @@
+﻿using DoubleDouble;
+using DoubleDoubleComplex;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DoubleDoubleComplexTests {
+    [TestClass()]
+    public class QuaternionSinhCoshTests {
+
+        [TestMethod()]
+        public void SinhCoshTest() {
+            foreach (Quaternion q in new[] { 1, 2, 4, 5 }) {
+                Quaternion s = Quaternion.Sinh(q), c = Quaternion.Cosh(q);
+
+                QuaternionAssert.AreEqual(ddouble.Sinh(q.R), s.R, 1e-25);
+                QuaternionAssert.AreEqual(ddouble.Cosh(q.R), c.R, 1e-25);
+            }
+
+            foreach (Quaternion q in new[] { (1, 2, 3, 4), (2, 5, -2, 6), (6, -3, 1, 2), (7, -4, 5, 1), (3, -9, 2, 4), (7, 1, -3, 2), (-3, 5, 2, -1) }) {
+                Quaternion s = Quaternion.Sinh(q), c = Quaternion.Cosh(q);
+
+                QuaternionAssert.AreEqual(Quaternion.One, c * c - s * s, 1e-23);
+            }
+        }
+
+        [TestMethod()]
+        public void TanhTest() {
+            foreach (Quaternion q in new[] { 1, 2, 4, 5 }) {
+                Quaternion y = Quaternion.Tanh(q);
+
+                QuaternionAssert.AreEqual(ddouble.Tanh(q.R), y.R, 1e-25);
+            }
+
+            foreach (Quaternion q in new[] { (1, 2, 3, 4), (2, 5, -2, 6), (6, -3, 1, 2), (7, -4, 5, 1), (3, -9, 2, 4), (7, 1, -3, 2), (-3, 5, 2, -1) }) {
+                Quaternion s = Quaternion.Sinh(q), c = Quaternion.Cosh(q);
+                Quaternion t = Quaternion.Tanh(q);
+
+                QuaternionAssert.AreEqual(s / c, t, 1e-25);
+            }
+        }
+    }
+}
