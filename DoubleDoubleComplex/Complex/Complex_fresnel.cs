7674722070aa03f35e2accb724a6681c47c6d262
tk-yoshimura <yoshimura8192@gmail.com>
2024-09-15 03:03:58 +0900

    add: fresnel

---
+++ DoubleDoubleComplex/Complex/Complex_fresnel.cs
@@ -0,0 +1,41 @@
+﻿using DoubleDouble;
+
+namespace DoubleDoubleComplex {
+
+    public partial class Complex {
+
+        public static Complex FresnelS(Complex z) {
+            if (!ddouble.IsFinite(z.R) || double.Abs((double)z.I) <= double.Abs((double)z.R) * 5e-31) {
+                return ddouble.FresnelS(z.R);
+            }
+
+            Complex y = (0.25d, 0.25d) * (
+                Erf(Consts.Fresnel.CPlus * z) -
+                (0d, 1d) * Erf(Consts.Fresnel.CMinus * z)
+            );
+
+            return y;
+        }
+
+        public static Complex FresnelC(Complex z) {
+            if (!ddouble.IsFinite(z.R) || double.Abs((double)z.I) <= double.Abs((double)z.R) * 5e-31) {
+                return ddouble.FresnelC(z.R);
+            }
+
+            Complex y = (0.25d, -0.25d) * (
+                Erf(Consts.Fresnel.CPlus * z) +
+                (0d, 1d) * Erf(Consts.Fresnel.CMinus * z)
+            );
+
+            return y;
+        }
+
+        internal static partial class Consts {
+            public static class Fresnel {
+                public static readonly ddouble SqrtPID2 = (+1, -1, 0xE2DFC48DA77B553CuL, 0xE1D82906AEDC9C1FuL);
+                public static readonly Complex CPlus = new Complex(1, 1) * SqrtPID2;
+                public static readonly Complex CMinus = new Complex(1, -1) * SqrtPID2;
+            }
+        }
+    }
+}
---
+++ DoubleDoubleComplexTest/Complex/ComplexFresnelTests.cs
@@ -0,0 +1,75 @@
+﻿using DoubleDoubleComplex;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DoubleDoubleComplexTests {
+
+    [TestClass()]
+    public class ComplexFresnelTests {
+
+        [TestMethod()]
+        public void FresnelSTest() {
+            ComplexAssert.AreEqual(
+                "0.4382591473903547660767566966251526374937865724524165673344073262",
+                Complex.FresnelS((1, 0)),
+                1e-28
+            );
+
+            ComplexAssert.AreEqual(
+                "-2.061888219194840468080716536685708600816+2.061888219194840468080716536685708600816i",
+                Complex.FresnelS((1, 1)),
+                1e-28
+            );
+
+            ComplexAssert.AreEqual(
+                "-2.061888219194840468080716536685708600816-2.061888219194840468080716536685708600816i",
+                Complex.FresnelS((1, -1)),
+                1e-28
+            );
+
+            ComplexAssert.AreEqual(
+                "2.061888219194840468080716536685708600816+2.061888219194840468080716536685708600816i",
+                Complex.FresnelS((-1, 1)),
+                1e-28
+            );
+
+            ComplexAssert.AreEqual(
+                "2.061888219194840468080716536685708600816-2.061888219194840468080716536685708600816i",
+                Complex.FresnelS((-1, -1)),
+                1e-28
+            );
+        }
+
+        [TestMethod()]
+        public void FresnelCTest() {
+            ComplexAssert.AreEqual(
+                "0.7798934003768228294742064136526901366306257081363209601031335831",
+                Complex.FresnelC((1, 0)),
+                1e-28
+            );
+
+            ComplexAssert.AreEqual(
+                "2.555793778102439024634522388352195842157+2.555793778102439024634522388352195842157i",
+                Complex.FresnelC((1, 1)),
+                1e-28
+            );
+
+            ComplexAssert.AreEqual(
+                "2.555793778102439024634522388352195842157-2.555793778102439024634522388352195842157i",
+                Complex.FresnelC((1, -1)),
+                1e-28
+            );
+
+            ComplexAssert.AreEqual(
+                "-2.555793778102439024634522388352195842157+2.555793778102439024634522388352195842157i",
+                Complex.FresnelC((-1, 1)),
+                1e-28
+            );
+
+            ComplexAssert.AreEqual(
+                "-2.555793778102439024634522388352195842157-2.555793778102439024634522388352195842157i",
+                Complex.FresnelC((-1, -1)),
+                1e-28
+            );
+        }
+    }
+}
