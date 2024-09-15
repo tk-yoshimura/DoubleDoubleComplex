57488a53e21f6e0f8aa223bf1cc57e962cda49e2
tk-yoshimura <yoshimura8192@gmail.com>
2024-09-16 03:46:06 +0900

    add: arsinh, arcosh, artanh

---
+++ DoubleDoubleComplex/Complex/Complex_arsinharcosh.cs
@@ -0,0 +1,25 @@
+﻿using DoubleDouble;
+
+namespace DoubleDoubleComplex {
+
+    public partial class Complex {
+
+        public static Complex Arsinh(Complex z) {
+            return ImaginaryOne * Asin(-ImaginaryOne * z);
+        }
+
+        public static Complex Arcosh(Complex z) {
+            Complex y = ImaginaryOne * Acos(z);
+            
+            if (ddouble.IsNegative(y.R)) {
+                y = -y;
+            }
+
+            return y;
+        }
+
+        public static Complex Artanh(Complex z) {
+            return -ImaginaryOne * Atan(ImaginaryOne * z);
+        }
+    }
+}
---
+++ DoubleDoubleComplexTest/Complex/ComplexArsinhArcoshTests.cs
@@ -0,0 +1,40 @@
+﻿using DoubleDoubleComplex;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DoubleDoubleComplexTests {
+    using NComplex = System.Numerics.Complex;
+
+    [TestClass()]
+    public class ComplexArsinhArcoshTests {
+
+        [TestMethod()]
+        public void ArsinhTest() {
+            foreach (Complex z in new[] { (1, 2), (2, 5), (6, -3), (7, -4), (3, -9), (7, 1), (-3, -4), (-1, -9), (-2, 1) }) {
+                Complex c = Complex.Arsinh(z);
+                Complex d = Complex.Sinh(c);
+
+                ComplexAssert.AreEqual(z, d, 1e-28);
+            }
+        }
+
+        [TestMethod()]
+        public void ArcoshTest() {
+            foreach (Complex z in new[] { (1, 2), (2, 5), (6, -3), (7, -4), (3, -9), (7, 1), (-3, -4), (-1, -9), (-2, 1) }) {
+                Complex c = Complex.Arcosh(z);
+                Complex d = Complex.Cosh(c);
+
+                ComplexAssert.AreEqual(z, d, 1e-28);
+            }
+        }
+
+        [TestMethod()]
+        public void ArtanhTest() {
+            foreach (Complex z in new[] { (1, 2), (2, 5), (6, -3), (7, -4), (3, -9), (7, 1), (-3, -4), (-1, -9), (-2, 1) }) {
+                Complex c = Complex.Artanh(z);
+                Complex d = Complex.Tanh(c);
+
+                ComplexAssert.AreEqual(z, d, 1e-28);
+            }
+        }
+    }
+}
