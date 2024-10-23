e8b9d199ae10b320ecd3f87bb11e3bf24a5a07c1
tk-yoshimura <yoshimura8192@gmail.com>
2024-10-23 18:22:29 +0900

    fix: bessel

--- DoubleDoubleComplex/Complex/Complex_bessel.cs
+++ DoubleDoubleComplex/Complex/Complex_bessel.cs
@@ -2421,7 +2421,8 @@ namespace DoubleDoubleComplex {
                         }
 
                         long exp_sum = 0;
-                        (Complex j0, Complex j1) = (s.Magnitude > 1d) ? (One, 1d / s) : (s, 1d);
+                        bool s_overone = s.Magnitude > 1d;
+                        (Complex j0, Complex j1) = s_overone ? (One, 1d / s) : (s, 1d);
 
                         for (int k = n - 1; k >= DirectMaxN - 1; k--) {
                             (j1, j0) = (ddouble.Ldexp(k + alpha, 1) * v * j1 - j0, j1);
@@ -2439,7 +2440,7 @@ namespace DoubleDoubleComplex {
                             ? Complex.BesselJ(alpha + (DirectMaxN - 1), z) / j0
                             : Complex.BesselJ(alpha + (DirectMaxN - 2), z) / j1,
                             (int)long.Clamp(-exp_sum, int.MinValue, int.MaxValue)
-                        ) * ((s.Magnitude > 1d) ? 1d : s);
+                        ) * (s_overone ? 1d : s);
 
                         return y;
                     }
@@ -2519,21 +2520,26 @@ namespace DoubleDoubleComplex {
                     }
 
                     long exp_sum = 0;
-                    Complex i0 = 1d, i1 = 1d / s;
+                    bool s_overone = s.Magnitude > 1d;
+                    (Complex i0, Complex i1) = s_overone ? (One, 1d / s) : (s, 1d);
 
-                    for (int k = n - 1; k >= DirectMaxN; k--) {
+                    for (int k = n - 1; k >= DirectMaxN - 1; k--) {
                         (i1, i0) = (ddouble.Ldexp(k + alpha, 1) * v * i1 + i0, i1);
 
-                        if (ILogB(i1) > 0) {
-                            int exp = ILogB(i1);
+                        int j0_exp = ILogB(i0), j1_exp = ILogB(i1);
+                        if (int.Sign(j0_exp) * int.Sign(j1_exp) > 0) {
+                            int exp = j0_exp > 0 ? int.Max(j0_exp, j1_exp) : int.Min(j0_exp, j1_exp);
                             exp_sum += exp;
                             (i0, i1) = (Ldexp(i0, -exp), Ldexp(i1, -exp));
                         }
                     }
 
-                    Complex y = Complex.BesselI(alpha + (DirectMaxN - 1), z) / i1;
-
-                    y = Ldexp(y, (int)long.Max(-exp_sum, int.MinValue));
+                    Complex y = Ldexp(
+                        i0.Magnitude >= i1.Magnitude
+                        ? Complex.BesselI(alpha + (DirectMaxN - 1), z) / i0
+                        : Complex.BesselI(alpha + (DirectMaxN - 2), z) / i1,
+                        (int)long.Clamp(-exp_sum, int.MinValue, int.MaxValue)
+                    ) * (s_overone ? 1d : s);
 
                     if (ddouble.IsNegative(nu) && !ddouble.IsInteger(nu_abs)) {
                         Complex bk = 2d * ddouble.RcpPI * SinCosPICache.SinPI(nu_abs) * Complex.BesselK(nu_abs, z);
--- DoubleDoubleComplex/Util/SeriesUtil.cs
+++ DoubleDoubleComplex/Util/SeriesUtil.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using DoubleDouble;
+using System.Runtime.CompilerServices;
 
 namespace DoubleDoubleComplex {
     internal static class SeriesUtil {
@@ -13,9 +14,16 @@ namespace DoubleDoubleComplex {
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Complex Add(Complex c, Complex s, Complex a, Complex b, out bool convergence) {
-            Complex c_next_x = c + s * (a + b), c_next_y = c + s * (a - b);
+            Complex c_next_x = c + s * (a + b);
 
-            convergence = (c == c_next_x) && (c == c_next_y);
+            if (c != c_next_x) {
+                convergence = false;
+                return c_next_x;
+            }
+
+            Complex c_next_y = c + s * (a - b);
+
+            convergence = c == c_next_y;
 
             return c_next_x;
         }
