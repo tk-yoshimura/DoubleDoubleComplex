c6b66f9e15bd59bbf838669ce7df237d390e4978
tk-yoshimura <yoshimura8192@gmail.com>
2024-10-29 19:07:02 +0900

    Update AssemblyInfo.cs

--- DoubleDoubleComplex/Properties/AssemblyInfo.cs
+++ DoubleDoubleComplex/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("1439DF1F-51EC-4989-8686-FB8526A33CE7")]
 
-[assembly: AssemblyVersion("1.5.6.*")]
+[assembly: AssemblyVersion("1.5.7.*")]
 
 [assembly: InternalsVisibleTo("DoubleDoubleComplexTest")]
 [assembly: InternalsVisibleTo("DoubleDoubleComplexSandbox")]
