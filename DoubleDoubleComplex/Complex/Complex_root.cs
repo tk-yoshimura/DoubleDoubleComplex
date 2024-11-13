0c525e63c8f6a7e7648566021c6a034fdca55466
tk-yoshimura <yoshimura8192@gmail.com>
2024-11-13 19:27:44 +0900

    refactor

--- DoubleDoubleComplex/Complex/Complex_root.cs
+++ DoubleDoubleComplex/Complex/Complex_root.cs
@@ -5,11 +5,11 @@ namespace DoubleDoubleComplex {
     public partial class Complex {
 
         public static Complex Sqrt(Complex z) {
-            return FromPolarCoordinates(ddouble.Sqrt(z.Magnitude), z.Phase / 2);
+            return FromPolarCoordinates(ddouble.Sqrt(z.Magnitude), z.Phase / 2d);
         }
 
         public static Complex Cbrt(Complex z) {
-            return FromPolarCoordinates(ddouble.Cbrt(z.Magnitude), z.Phase / 3);
+            return FromPolarCoordinates(ddouble.Cbrt(z.Magnitude), z.Phase / 3d);
         }
 
         public static Complex RootN(Complex z, int n) {
--- DoubleDoubleComplex/Quaternion/Quaternion_sinhcosh.cs
+++ DoubleDoubleComplex/Quaternion/Quaternion_sinhcosh.cs
@@ -5,13 +5,13 @@
         public static Quaternion Sinh(Quaternion q) {
             Quaternion q_pexp = Exp(q), q_mexp = Exp(-q);
 
-            return (q_pexp - q_mexp) / 2d;
+            return Ldexp(q_pexp - q_mexp, -1);
         }
 
         public static Quaternion Cosh(Quaternion q) {
             Quaternion q_pexp = Exp(q), q_mexp = Exp(-q);
 
-            return (q_pexp + q_mexp) / 2d;
+            return Ldexp(q_pexp + q_mexp, -1);
         }
 
         public static Quaternion Tanh(Quaternion q) {
