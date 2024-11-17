abb0fa74a8a1aae9969b77bc8e9ad2589ebc989c
tk-yoshimura <yoshimura8192@gmail.com>
2024-11-18 00:46:11 +0900

    cleanup

--- DoubleDoubleComplexTest/Octonion/OctonionIOTests.cs
+++ DoubleDoubleComplexTest/Octonion/OctonionIOTests.cs
@@ -1,7 +1,6 @@
 using DoubleDouble;
 using DoubleDoubleComplex;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
 using System.IO;
 
 namespace DoubleDoubleComplexTests {
